18b3e3d578aa9557e823057efccaf4d158e1b5cc
DylanPatrascu <dylanpatrascu1@gmail.com>
2025-03-26 16:33:43 -0400

    Updated Collection

---
+++ Assets/Scripts/OnButtonHover.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+
+public class OnButtonHover : MonoBehaviour
+{
+    public Color hoverColor = new Color(200f / 255f, 170f / 255f, 110f / 255f);
+    public Color defaultColor = new Color(200f / 255f, 155f / 255f, 60f / 255f);
+    public int fontSizeIncrease = 5;
+
+    public void UpdateTextEnter(TMP_Text buttonText)
+    {
+        buttonText.fontSize += fontSizeIncrease;
+        buttonText.color = hoverColor;
+    }
+
+    public void UpdateTextExit(TMP_Text buttonText)
+    {
+        buttonText.fontSize -= fontSizeIncrease;
+        buttonText.color = defaultColor;
+    }
+}
