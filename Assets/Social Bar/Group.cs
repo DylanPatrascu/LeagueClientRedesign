ded0a7fff17fd824d7a20645e5cb622b2773d28f
samymillion <samuelgebretsion@gmail.com>
2025-03-31 08:15:24 -0400

    Updated to work with all current scenes
    Linked all scenes up to this point

--- Assets/Social Bar/Group.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewGroup", menuName = "Group")]
-public class Group : ScriptableObject
-{
-    public User[] users;
-    public int messagingUserIndex;
-    public string groupName, message;
-}
--- Assets/Social Bar/User.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewUser", menuName = "User")]
-public class User : ScriptableObject
-{
-    public string username, message;
-    public Sprite pfp;
-}
